7ce05783a2b41e4e6c72f7fecf7b799176f7f1f8
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2017-11-07 13:41:45 +0300

    Replace rbenv by asdf

--- config.fish
+++ config.fish
@@ -47,14 +47,15 @@ end
 # Set environment variables
 ################################################################################
 set -x EDITOR nvim
+set -x PAGER most
 set -x PATH $PATH $HOME/bin
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x FZF_DEFAULT_OPTS '--height 40% --reverse'
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
-# Init rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
 # Configure direnv
 eval (direnv hook fish)
+
+# Setup asdf
+source ~/.asdf/asdf.fish
