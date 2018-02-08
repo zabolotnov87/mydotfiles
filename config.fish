94941d30174332196e1613dd1b19b43b19b5ab5f
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2018-02-09 01:54:31 +0400

    Remove unused

--- config.fish
+++ config.fish
@@ -9,7 +9,6 @@ alias tmux "env TERM=xterm-256color tmux"
 alias spec "bundle exec rescue rspec"
 alias vim  "nvim"
 alias vi   "nvim"
-alias als  "grep -nr alias ~/.config/fish/config.fish | cut -d ':' -f 3"
 alias lfc  "source ~/.config/fish/config.fish"
 
 # Git
