85ada88350853043c2936a741dd48ddbbbb7e265
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2019-05-28 21:28:30 +0300

    Remove GO settings

--- config.fish
+++ config.fish
@@ -52,10 +52,7 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 set -x FZF_DEFAULT_OPTS "--reverse $FZF_DEFAULT_OPTS"
-set -x GOPATH $HOME/go
-set -x GOBIN $GOPATH/bin
 set -x PATH $PATH $HOME/bin $GOBIN
-set -x GOROOT /Users/zabolotnov/.asdf/installs/golang/1.9.4/go
 set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
 set -x DIRENV_LOG_FORMAT ""
 
