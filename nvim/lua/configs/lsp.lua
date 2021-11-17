3c12bcbf80f66a0bbd107060e3e86f7f1c47dc5e
Sergey Zaboltonov <sergey.zabolotnov@gmail.com>
2021-11-18 01:02:24 +0300

    Exclude solargraph from lsp servers list

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -27,13 +27,13 @@ local on_attach = function(client, bufnr)
   local opts = { noremap=true, silent=true }
 
   buf_set_keymap('n', '<leader>ld',  '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', ']e',          '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', ']d',          '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K',           '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', '<leader>li',  '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', '<leader>lq',  '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap('n', '<leader>le',  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d',          '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d',          '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '[e',          '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']e',          '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<leader>la',  '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', '<leader>lf',  '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
   buf_set_keymap('n', '<leader>ls',  '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -42,13 +42,13 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<leader>lwl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   buf_set_keymap('n', ']t',          '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', '<leader>lr',  '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>lR',   '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<leader>lR',  '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = { 'solargraph', 'tsserver', 'gopls' }
+local servers = { 'tsserver', 'gopls' }
 for _, server in ipairs(servers) do
   lsp[server].setup {
     on_attach = on_attach,
